8ec00bc507e2afd4b08eec26077b2b9093c36026
contributorsambhav <sambhav511974@gmail.com>
2024-09-01 22:18:34 +0530

    ..

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
+const PORT =3000;
 const app = express();
 dotenv.config();
 await connectDB();
